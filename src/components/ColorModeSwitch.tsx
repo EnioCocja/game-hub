405e296f9dfbf606c164d37ede5ce852257f3dfd
Enio Cocja <eniococja123@gmail.com>
2023-10-15 11:41:00 +0200

    Add Icon for color mode

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -1,15 +1,21 @@
 import { HStack, Switch, Text, useColorMode } from "@chakra-ui/react";
+import { BsMoonFill, BsSun, BsSunFill } from "react-icons/bs";
 
 const ColorModeSwitch = () => {
   const { toggleColorMode, colorMode } = useColorMode();
   return (
     <HStack>
+      <Text whiteSpace={"nowrap"}>
+        <BsSunFill />
+      </Text>
       <Switch
         colorScheme="green"
         isChecked={colorMode === "dark"}
         onChange={toggleColorMode}
       />
-      <Text whiteSpace={"nowrap"}>Dark Mode</Text>
+      <Text whiteSpace={"nowrap"}>
+        <BsMoonFill />
+      </Text>
     </HStack>
   );
 };
