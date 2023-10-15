c162f443cde360befd668f7f3cfb66ed0695190d
Enio Cocja <eniococja123@gmail.com>
2023-10-15 11:43:21 +0200

    Reset icons for color mode

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -1,21 +1,15 @@
 import { HStack, Switch, Text, useColorMode } from "@chakra-ui/react";
-import { BsMoonFill, BsSun, BsSunFill } from "react-icons/bs";
 
 const ColorModeSwitch = () => {
   const { toggleColorMode, colorMode } = useColorMode();
   return (
     <HStack>
-      <Text whiteSpace={"nowrap"}>
-        <BsSunFill />
-      </Text>
       <Switch
         colorScheme="green"
         isChecked={colorMode === "dark"}
         onChange={toggleColorMode}
       />
-      <Text whiteSpace={"nowrap"}>
-        <BsMoonFill />
-      </Text>
+      <Text whiteSpace={"nowrap"}>Dark Mode</Text>
     </HStack>
   );
 };
